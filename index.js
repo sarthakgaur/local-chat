b2aa3b0deb7a26c8a64535b1d7b294bda01d0416
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2021-01-23 16:16:29 +0530

    Added support for nicknames

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+// TODO Add support for nicknames
+
+let connections = new Map();
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on('username', (username) => {
+    connections.set(socket.id, username);
+  });
+
+  socket.on('chat message', (body) => {
+    let time = Date.now();
+    let username = connections.get(socket.id);
+
+    io.emit('chat message', { time, username, body });
   });
 });
 
