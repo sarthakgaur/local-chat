7ff66105e90a73944026be75aa570a24c1fe6403
sarthakgaur <75014896+sarthakgaur@users.noreply.github.com>
2021-01-24 14:32:59 +0530

    Files can now be uploaded to the server
    If the file is an image, it is displayed in the chat window. The input
    file button is also style now.

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const fs = require('fs');
 
 // TODO Add support for nicknames. Done.
 // TODO Broadcast message when a user connects or disconnects. Done.
 // TODO Add option to get a list of connected users. Done
 // TODO Fix the layout in mobile devices. Done.
 // TODO Save username in the localStorage. Done.
+// TODO Allow uploading of files. Done.
+// TODO View images in chat. Done.
+// TODO Fix upload button. Done.
+
+app.use(express.static(__dirname + '/public'));
 
 let connections = new Map();
 
@@ -30,6 +37,17 @@ io.on('connection', (socket) => {
     let username = connections.get(socket.id);
     connections.delete(socket.id);
     io.emit('userDisconnected', { username, userList: getUsersList() });
+  });
+
+  socket.on('fileUpload', (file) => {
+    let time = Date.now();
+    let username = connections.get(socket.id);
+    fs.promises.writeFile(`./public/${file.name}`, file.buffer)
+      .then(() => {
+        let link = `/${file.name}`;
+        io.emit('fileUpload', { time, username, link, type: file.type });
+      })
+      .catch(console.error)
   });
 });
 
